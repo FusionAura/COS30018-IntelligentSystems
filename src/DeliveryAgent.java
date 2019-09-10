1943219012becf817d7f5da47bea25dee2041089
dylros98 <101624317@student.swin.edu.au>
2019-09-10 17:13:21 +1000

    Added basic MasterRoutingAgent and DeliveryAgent which send messages to each other

--- src/DeliveryAgent.java
+++ src/DeliveryAgent.java
@@ -1,2 +1,26 @@
-public class DeliveryAgent {
+import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.lang.acl.*;
+
+public class DeliveryAgent extends Agent
+{
+    protected void setup()
+    {
+        addBehaviour(new CyclicBehaviour(this)
+        {
+            public void action()
+            {
+                ACLMessage msg = receive();
+                if(msg!=null)
+                {
+                    System.out.println(getLocalName()+" Received message "+msg.getContent()+" from "+msg.getSender().getLocalName());
+                    ACLMessage reply = msg.createReply();
+                    reply.setPerformative(ACLMessage.INFORM);
+                    reply.setContent("Agent "+getLocalName()+" responding!");
+                    System.out.println(getLocalName()+": Sending response "+reply.getContent()+" to "+msg.getAllReceiver().next());
+                    send(reply);
+                }
+            }
+        });
+    }
 }
