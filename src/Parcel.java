1591d7425b46ca740f923bb177ff50e83b10560f
Xavier Gibbon <101104563@student.swin.edu.au>
2019-09-16 20:59:35 +1000

    Added Node and Parcel classes

--- src/Parcel.java
+++ src/Parcel.java
@@ -1,2 +1,17 @@
 public class Parcel {
+    private String _destination;
+    private int _weight;
+
+    public Parcel(String destination, int weight) {
+        _destination = destination;
+        _weight = weight;
+    }
+
+    public String getDestination() {
+        return _destination;
+    }
+
+    public int getWeight() {
+        return _weight;
+    }
 }
