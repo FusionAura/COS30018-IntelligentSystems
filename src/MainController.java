b9c53c2f0be7dd2bc4a306ba4728c3916122d107
Stewart <grant-stewart@hotmail.com>
2019-09-25 23:16:21 +1000

    Loads GUI now. need to populate it

--- src/MainController.java
+++ src/MainController.java
@@ -1,16 +1,38 @@
+
+
 import jade.core.Runtime;
 import jade.core.Profile;
 import jade.core.ProfileImpl;
 import jade.wrapper.*;
-public class MainController
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import javafx.scene.control.ListView;
+
+public class MainController extends Application
 {
-    public static void main (String[] args) throws StaleProxyException, InterruptedException
+    //List View variables.
+    @FXML private ListView AgentsList;
+    @FXML private Button CreateButton;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
     {
+        Parent root = FXMLLoader.load(getClass().getResource("IntelligentSystems.fxml"));
+        primaryStage.setTitle("Intelligent Systems Agent Program");
+        primaryStage.setScene(new Scene (root));
+        primaryStage.show();
         Runtime rt= Runtime.instance();
 
         System.out.println(MainController.class.getName() + ": Launching the platform Main Container...");
         Profile pMain= new ProfileImpl(null, 8888, null);
-        pMain.setParameter(Profile.GUI, "true");
+        //pMain.setParameter(Profile.GUI, "true");
         ContainerController mainCtrl = rt.createMainContainer(pMain);
 
         // Create and start an agent of class Counter Agent
@@ -28,5 +50,15 @@ public class MainController
         AgentController agentCtrl= mainCtrl.createNewAgent("MasterRoutingAgent", MasterRoutingAgent.class.getName(), new Object[0]);
         agentCtrl.start();
 
+        //Populate List
+        //AgentsList.getItems().addAll("");
+
+
     }
+
+    public static void main (String[] args) throws StaleProxyException
+    {
+        launch(args);
+    }
+
 }
