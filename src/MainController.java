8e6459ed69d1aadac24b29fb2902213e1b79f3bd
Stewart <grant-stewart@hotmail.com>
2019-10-01 15:54:17 +1000

    GUI

--- src/MainController.java
+++ src/MainController.java
@@ -1,38 +1,54 @@
-
-
 import jade.core.Runtime;
 import jade.core.Profile;
 import jade.core.ProfileImpl;
 import jade.wrapper.*;
-
 import javafx.application.Application;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
-import javafx.scene.control.ListView;
+import javafx.event.EventHandler;
+import javafx.stage.WindowEvent;
 
 public class MainController extends Application
 {
+    @FXML
+    public ListView AgentsList;
+
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        Parent root = FXMLLoader.load(getClass().getResource("IntelligentSystems.fxml"));
+        //Load GUI
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("IntelligentSystems.fxml"));
+        Parent root = loader.load(); // must be called before getting the controller!
+        GUIController controller = loader.getController();
         primaryStage.setTitle("Intelligent Systems Agent Program");
         primaryStage.setScene(new Scene (root));
         primaryStage.show();
-        Runtime rt= Runtime.instance();
 
+        //Close Button
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent we) {
+                System.out.println("Stage is closing");
+                System.exit(0);
+            }
+        });
+
+        //JADE Startup
+        Runtime rt= Runtime.instance();
         System.out.println(MainController.class.getName() + ": Launching the platform Main Container...");
         Profile pMain= new ProfileImpl(null, 8888, null);
         //pMain.setParameter(Profile.GUI, "true");
         ContainerController mainCtrl = rt.createMainContainer(pMain);
 
+
         // Create and start an agent of class Counter Agent
         System.out.println(MainController.class.getName() + ": Starting up the Master Controller...");
+
+        AgentController agentCtrl= mainCtrl.createNewAgent("MasterRoutingAgent", MasterRoutingAgent.class.getName(), new Object[0]);
+        agentCtrl.start();
 
         AgentController DeliveryAgent= mainCtrl.createNewAgent("d1", DeliveryAgent.class.getName(), new Object[0]);
         DeliveryAgent.start();
@@ -43,12 +59,16 @@ public class MainController extends Application
         AgentController DeliveryAgent3= mainCtrl.createNewAgent("d3", DeliveryAgent.class.getName(), new Object[0]);
         DeliveryAgent3.start();
 
-        AgentController agentCtrl= mainCtrl.createNewAgent("MasterRoutingAgent", MasterRoutingAgent.class.getName(), new Object[0]);
-        agentCtrl.start();
 
-        //Populate List
-        //AgentsList.getItems().addAll("");
+        //Populate GUI ListView
+        controller.DoList.add(agentCtrl.getName());
+        controller.DoList.add(DeliveryAgent.getName());
+        controller.DoList.add(DeliveryAgent2.getName());
+        controller.DoList.add(DeliveryAgent3.getName());
+        controller.PopulateAgentList();
 
+        controller.MainClass = this;
+        controller.AgentNum.setText(String.valueOf(controller.DoList.size()-1));
 
     }
 
@@ -56,5 +76,4 @@ public class MainController extends Application
     {
         launch(args);
     }
-
 }
